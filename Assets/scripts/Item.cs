aa14bb8c0827d61725a572a7cf18cf9a975d501f
mbahrami1 <bahramikhajouei1353@gmail.com>
2025-07-14 17:25:12 +0330

    commit 5

--- Assets/scripts/Item.cs
+++ Assets/scripts/Item.cs
@@ -2,12 +2,29 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+    public GameObject itemPrefab;
+    public float respawnHeight = 5f;
+    public float respawnRangeX = 4.5f;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
         
+            RespawnItem();
+
+        
             Destroy(gameObject);
         }
+    }
+
+    void RespawnItem()
+    {
+        
+        float randomX = Random.Range(-respawnRangeX, respawnRangeX);
+        Vector3 newPosition = new Vector3(randomX, respawnHeight, 0f);
+
+        
+        Instantiate(itemPrefab, newPosition, Quaternion.identity);
     }
 }
